3a214417691ccf681cb2af3ecfbed893820ae886
strichard0304 <ume_li@seed.net.tw>
2016-06-02 21:58:43 +0800

    0602

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -3,17 +3,46 @@ package com.example.user.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editText;
+    String selectedSex = "Male";
+    String name = "";
+    CheckBox checkBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Debug", "Hello log");
+
         textView = (TextView) findViewById(R.id.TestView);
         textView.setText("Hello TextView");
+        editText = (EditText)findViewById(R.id.editText);
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+
+                return false;
+            }
+        });
+        checkBox = (CheckBox)findViewById(R.id.chechBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+    }
+    public void click(View view){
+        textView.setText("Press button");
     }
 }
